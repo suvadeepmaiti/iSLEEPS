37f53944eecee6d5c8cb6534bd5d8912823c1d4f
Saswata Bose <67733706+HimalayanSaswataBose@users.noreply.github.com>
2025-08-11 16:36:08 +0530

    Anonymity in file paths

--- iSLEEPS_preprocess_v2/config.py
+++ iSLEEPS_preprocess_v2/config.py
@@ -1,10 +1,10 @@
 # Path to the raw data directory
 # This directory contains the raw sleep (EDF and .xlsx) data files that need to be processed.
-raw_file_path = '/scratch/saswatabose/sleep_data/sample'
+raw_file_path = '<PATH>'
 
 # Path to the output directory where preprocessed data will be saved
 # After processing, the cleaned and transformed data will be stored in this directory.
-output_data_path = '/scratch/saswatabose/nimhans_preprocessed_sample/'
+output_data_path = '<PATH>'
 
 # List of modalities to be processed
 # Specifies the types of data modalities to process. In this case, we are working with EEG and EOG data.
